cc9c8651a0166cbc40918eb00cae3c905be957bc
pants64DS <pants64DS@users.noreply.github.com>
2022-11-21 21:49:16 +0200

    Start implementing the GUI

---
+++ src/index.py
@@ -0,0 +1,15 @@
+from tkinter import Tk
+from ui import UI
+
+def main():
+    window = Tk()
+    window.title("Ohjelmoijan laskin")
+    window.geometry("500x300")
+
+    ui = UI(window)
+    ui.start()
+
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
